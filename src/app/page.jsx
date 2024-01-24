98aeb3f2f02f89f50a3166b454346c91b770e0e7
Savchenko-Andrey <dracxm95@gmail.com>
2024-01-24 14:26:22 +0200

    carusel section partner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 
-
 import Hero from "@/libs/components/hero/Hero";
 import NewDevelopment from '@/libs/components/newDevelopment/newDevelopment'
 import About from "@/libs/components/about/about";
 import Description from "@/libs/components/description/description";
 import Contacts from "@/libs/components/section-contacts/section-contacts";
+import Partners from "@/libs/components/partners/partners";
 
 
 export default async function Home() {
@@ -13,6 +13,7 @@ export default async function Home() {
       <Hero />
       <NewDevelopment />
       <About />
+      <Partners />
       <Description />
       <Contacts />
     </>
