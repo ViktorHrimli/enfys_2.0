99c1d0cd15404a0247f6c00ab66fad1f46a44cca
Savchenko-Andrey <dracxm95@gmail.com>
2024-03-11 13:49:10 +0200

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+
+import Hero from "@/libs/components/hero/Hero";
+import AboutStore from "@/libs/pages/components/about-store/about-store";
+
+
+export default async function Home() {
+  return (
+    <>
+      <Hero />
+      <AboutStore />
+    </>
+  );
+}
