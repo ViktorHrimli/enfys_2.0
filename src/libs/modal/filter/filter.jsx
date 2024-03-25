5258d99ba617bc79d51042e48487ea7acb002cd4
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-03-25 21:56:49 +0100

    add closed+filter

--- src/libs/modal/filter/filter.jsx
+++ src/libs/modal/filter/filter.jsx
@@ -4,20 +4,22 @@ export default function Filter({ }) {
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
+      <div className={styles.closed}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="white" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"/></svg>
+      </div>
         <div className={styles.container_filter}>
           <div className={styles.left_box}>
             <div className={styles.sorting_box}>
               <p className={styles.sorting_text}>СОРТУВАТИ ЗА</p>
-              <input placeholder="Не обрано" name="sorting" list="sortings" />
-<select id="sortings">
-   <option value="від дешевих додорогих" />
-   <option value="від дорогих до дешевих" />
+<select id="sortings" className={`${styles.select} ${styles.style_input}`}>
+   <option value="від дешевих додорогих" >від дешевих додорогих</option>
+   <option value="від дорогих до дешевих" >від дорогих до дешевих</option>
 </select>
             </div>
             <p>ЦІНА</p>
             <div className={styles.price_box}>
-              <div className={styles.price}>від <input type="number" /></div>
-              <div className={styles.price}>до <input type="number" /></div>
+              <div className={styles.price}>від <input className={styles.style_input} type="number" /></div>
+              <div className={styles.price}>до <input  className={styles.style_input}type="number" /></div>
             </div>
           </div>
           <div className={styles.right_box}>
@@ -43,7 +45,6 @@ export default function Filter({ }) {
     <input type="checkbox" id="NINOS" name="NINOS" />
     <label for="NINOS">NINOS</label>
   </div>
-         
         </div>
         </div>
           <div className={styles.btn_box}>
