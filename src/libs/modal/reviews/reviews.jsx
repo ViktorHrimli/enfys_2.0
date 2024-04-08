eab5990a931b95176e70d3d9d77329847610a6cf
Savchenko-Andrey <dracxm95@gmail.com>
2024-04-08 22:43:46 +0300

    fix

---
+++ src/libs/modal/reviews/reviews.jsx
@@ -0,0 +1,9 @@
+import styles from './reviews.module.scss'
+
+export default function Reviews() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}></div>
+    </div>
+  )
+}
