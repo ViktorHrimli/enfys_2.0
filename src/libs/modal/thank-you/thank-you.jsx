7f848d9b6d498681268bc7a51071c3184275c9ef
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-04-24 08:41:12 +0200

    add modal thank you

--- src/libs/modal/thank-you/thank-you.jsx
+++ src/libs/modal/thank-you/thank-you.jsx
@@ -15,6 +15,9 @@ export default function ThenkYou() {
               loading="eager"
             />
         </div>
+        <h2 className={styles.header}>дякуємо, ваш запит успішно надісланий</h2>
+        <p className={styles.inform}>Ми зв’яжемося з Вами якомога швидше. Гарного дня!</p>
+        <p className={styles.post_scriptum}>P.S. Пам’ятайте, ви - прекрасні батьки для своєї дитини.</p>
         <div>
           {/* qwertyui */}
         </div>
