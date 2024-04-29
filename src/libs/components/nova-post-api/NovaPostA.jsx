8e55cb564cece102151dcb57ee54a485b4191913
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2024-04-29 22:14:05 +0300

    make change post office

--- src/libs/components/nova-post-api/NovaPostA.jsx
+++ src/libs/components/nova-post-api/NovaPostA.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 const NovaPostA = () => {
   const [isCity, setisCity] = useState("");
   const [data, setData] = useState([]);
+  const [wareHouseRef, setWareHouseRef] = useState("");
+  const [postOffice, setPostOffice] = useState("");
   const [cityRef, setCityRef] = useState("");
 
   var useCity = async (city) => {
@@ -23,13 +25,44 @@ const NovaPostA = () => {
       method: "POST",
     });
     var { data } = await res.json();
+    console.log(data, "CITY");
     setData(data[0].Addresses);
   };
 
+  var getPostOffice = async (postOffice) => {
+    var obj = {
+      apiKey: process.env.NV_API_KEY,
+      modelName: "Address",
+      calledMethod: "getWarehouses",
+      methodProperties: {
+        FindByString: postOffice,
+        CityName: cityRef,
+        CityRef: "",
+        Page: "1",
+        Limit: "150",
+        Language: "UA",
+        TypeOfWarehouseRef: wareHouseRef,
+        WarehouseId: "",
+      },
+    };
+
+    var res = await fetch("https://api.novaposhta.ua/v2.0/json/", {
+      body: JSON.stringify(obj),
+      method: "POST",
+    });
+    var { data } = await res.json();
+    console.log(data, "OFFICE");
+  };
+
   useEffect(() => {
-    setTimeout(() => useCity(isCity), 300);
+    isCity && setTimeout(() => useCity(isCity), 300);
   }, [isCity]);
 
+  useEffect(() => {
+    cityRef && postOffice && setTimeout(() => getPostOffice(postOffice), 500);
+  }, [postOffice]);
+
+  console.log(cityRef, "REF");
   return (
     <section
       style={{
@@ -69,11 +102,12 @@ const NovaPostA = () => {
               display: "flex",
               flexDirection: "column",
             }}
-            defaultValue={"dadadad"}
+            onChange={(event) => setCityRef(event.target.value)}
+            value={cityRef}
           >
             {data.map((item, id) => (
-              <option key={id} value={item.Ref}>
-                {item.Present}
+              <option key={id} value={item.MainDescription}>
+                {item.MainDescription}
               </option>
             ))}
           </select>
@@ -81,7 +115,10 @@ const NovaPostA = () => {
 
         <label>
           Вибрати тип відділення
-          <select>
+          <select
+            onChange={(event) => setWareHouseRef(event.target.value)}
+            value={wareHouseRef}
+          >
             <option value="f9316480-5f2d-425d-bc2c-ac7cd29decf0">
               Поштомат
             </option>
@@ -92,6 +129,15 @@ const NovaPostA = () => {
               Грузове
             </option>
           </select>
+        </label>
+
+        <label>
+          Вибрати відділення
+          <input
+            type="text"
+            value={postOffice}
+            onChange={(event) => setPostOffice(event.target.value)}
+          />
         </label>
       </form>
     </section>
