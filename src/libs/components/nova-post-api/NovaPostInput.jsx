36cfd293c8aed4d111f370e06952257efae83bc7
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2024-05-07 20:47:13 +0300

    fix commit

--- src/libs/components/nova-post-api/NovaPostInput.jsx
+++ src/libs/components/nova-post-api/NovaPostInput.jsx
@@ -1,6 +1,7 @@
 const NovaPostInput = ({ value, onClick, children, ...rest }) => {
   return (
     <label>
+      {/* adada */}
       {children}
       <input type="text" {...rest} onClick={onClick} value={value} />
     </label>
