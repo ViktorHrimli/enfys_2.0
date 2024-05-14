68ec5a40f9cfe04b805d39fca8d6f3526e6480bd
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2024-05-15 00:11:20 +0300

    create fuccking payment

--- src/libs/components/liqpay/LiqpayCheckout.jsx
+++ src/libs/components/liqpay/LiqpayCheckout.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { LiqPay } from "./liqpay_sdk";
 const LiqpayCheckout = ({ props, html, ...rest }) => {
+  const [isOpenPay, setIsOpenPay] = useState(false);
   var func = () => {
     var liqpaySDK = new LiqPay(
       "sandbox_i53055371277",
@@ -12,57 +13,25 @@ const LiqpayCheckout = ({ props, html, ...rest }) => {
       action: "pay",
       public_key: "sandbox_i53055371277",
       amount: "50",
+      paytypes: "apay,gpay,moment_part,privat24,paypart,cash,invoice,qr",
       currency: "UAH",
       description: "description text",
       order_id: "123123",
       version: "3",
     });
 
-    console.log(html);
-
     return html;
   };
 
-  useEffect(() => {
-    var str = func();
-    document.getElementById("keku").innerHTML = str;
-  }, []);
   return (
-    <div
+    <section
       id="keku"
+      dangerouslySetInnerHTML={{ __html: func() }}
       style={{
-        display: "block",
+        display: "flex",
         marginTop: "50px",
-        height: "100%",
-        width: "100%",
       }}
-    >
-      <form
-        method="POST"
-        action="https://www.liqpay.ua/api/3/checkout"
-        accept-charset="utf-8"
-      >
-        <input
-          type="hidden"
-          name="data"
-          value="eyJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5Ijoic2FuZGJveF9pNTMwNTUzNzEyNzciLCJhbW91bnQiOjUwLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoiZGVzY3JpcHRpb24gdGV4dCIsIm9yZGVyX2lkIjoiMTIzMTIzIiwidmVyc2lvbiI6M30="
-        />
-        <input
-          type="hidden"
-          name="signature"
-          value="sjhM4ksRqbFEmNEu5Sm7fFxSQic="
-        />
-        <script
-          type="text/javascript"
-          src="https://static.liqpay.ua/libjs/sdk_button.js"
-        ></script>
-        <sdk-button
-          label="Сплатити"
-          background="#77CC5D"
-          onClick="submit()"
-        ></sdk-button>
-      </form>
-    </div>
+    ></section>
   );
 };
 
