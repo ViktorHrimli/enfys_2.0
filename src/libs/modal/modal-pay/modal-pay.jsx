5866144aa79966b610171e45b19a764461cba984
viktorhrimli <viktorhrimli101@gmail.com>
2024-05-28 22:32:50 +0300

    fix bag

--- src/libs/modal/modal-pay/modal-pay.jsx
+++ src/libs/modal/modal-pay/modal-pay.jsx
@@ -1,23 +1,10 @@
-"use client"
-import styles from './modal-pay.module.scss'
-import Link from 'next/link';
-import Item from './item/item';
+"use client";
+import styles from "./modal-pay.module.scss";
+import Link from "next/link";
+import Item from "./item/item";
 import { useState, useEffect } from "react";
 
-
-
 export default function PayModal({ setIsPay, storedItems, setStoredItems }) {
-  const [isData, setIsData] = useState([]);
-
-
-  useEffect(() => {
-    const localeData = JSON.parse(localStorage.getItem("storedItems"));
-
-    if (localeData) {
-      setIsData(localeData);
-    }
-  }, [])
-
   const totalCardPrice = storedItems.reduce((accumulator, item) => {
     return accumulator + parseFloat(item.PriceCards);
   }, 0);
@@ -26,44 +13,60 @@ export default function PayModal({ setIsPay, storedItems, setStoredItems }) {
     setIsPay(false);
   }
 
-  
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
-        <div className={styles.closed} onClick={()=> setIsPay(false)}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="white" d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"/></svg>
+        <div className={styles.closed} onClick={() => setIsPay(false)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z"
+            />
+          </svg>
         </div>
         <h2 className={styles.title}>корзина</h2>
         <div className={styles.list}>
           <div className={styles.item_description}>
             <p className={styles.item_text}>Кількість</p>
             <p className={styles.item_text}>Сума</p>
-          </div>  
-        <ul>
-            {isData.map(({ ImageCards, TitleCards, PriceCards, QuantityCards }, id) => {
-              return (
-                <li key={id} className={styles.item}>
-                  <Item 
-                    id={id}
-                    storedItems={storedItems }
-                    setStoredItems={setStoredItems}
-
-                    ImageCards={ImageCards}
-                    TitleCards={TitleCards}
-                    PriceCards={PriceCards}
-                    QuantityCards={QuantityCards}
-                  />
-                </li>
-            )
-          })}
-        </ul>
+          </div>
+          <ul>
+            {storedItems.map(
+              ({ ImageCards, TitleCards, PriceCards, QuantityCards }, id) => {
+                return (
+                  <li key={id} className={styles.item}>
+                    <Item
+                      id={id}
+                      storedItems={storedItems}
+                      setStoredItems={setStoredItems}
+                      ImageCards={ImageCards}
+                      TitleCards={TitleCards}
+                      PriceCards={PriceCards}
+                      QuantityCards={QuantityCards}
+                    />
+                  </li>
+                );
+              }
+            )}
+          </ul>
         </div>
         <div className={styles.result}>
           <p className={styles.text}>Підсумок</p>
           <p className={styles.text}>{totalCardPrice} гривень</p>
         </div>
-        <Link href="/oformlennya-zamovlennya" className={styles.btn} onClick={closed}>оформити замовлення</Link>
+        <Link
+          href="/oformlennya-zamovlennya"
+          className={styles.btn}
+          onClick={closed}
+        >
+          оформити замовлення
+        </Link>
       </div>
     </div>
-  )
+  );
 }
--- src/libs/pages/components/pay/pay.jsx
+++ src/libs/pages/components/pay/pay.jsx
@@ -1,28 +1,31 @@
-"use client"
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { Liqpay } from '@/libs/components/liqpay/Liqpay';
-import { MeMoNovaPost } from '@/libs/components/nova-post-api/NovaPostA';
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { Liqpay } from "@/libs/components/liqpay/Liqpay";
+import { MeMoNovaPost } from "@/libs/components/nova-post-api/NovaPostA";
 
-import styles from './pay.module.scss'
-import PayModal from '@/libs/modal/modal-pay/modal-pay';
-
+import styles from "./pay.module.scss";
+import PayModal from "@/libs/modal/modal-pay/modal-pay";
 
 export default function Pay() {
   const [storedItems, setStoredItems] = useState([]);
   const [isPay, setIsPay] = useState(false);
 
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: ''
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
   });
 
   useEffect(() => {
     var localeData = JSON.parse(localStorage.getItem("storedItems"));
     localeData && setStoredItems(localeData);
   }, []);
+
+  useEffect(() => {
+    setStoredItems(JSON.parse(localStorage.getItem("storedItems")));
+  }, [isPay]);
 
   // Обработчик изменения полей формы
   const handleChange = (e) => {
@@ -33,7 +36,7 @@ export default function Pay() {
   // Обработчик отправки формы
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submitted data:', formData);
+    console.log("Submitted data:", formData);
     // Тут можно добавить логику для отправки данных на сервер
   };
 
@@ -43,114 +46,130 @@ export default function Pay() {
 
   return (
     <>
-    <section className={styles.section}>
-      <div className={styles.container}>
-      <div className={styles.container_form}>
-          <h2 className={styles.title}>Ваші контактні дані</h2>
-          
-      <form onSubmit={handleSubmit} className={styles.form}>
-      <div className={styles.container_input }>
-        <input
-        className={styles.input}
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder='Ім’я'
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-        <input
-        className={styles.input}
-          type="text"
-          id="lastName"
-          name="lastName"
-          placeholder='Прізвище'
-                
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-      </div>
-      <div className={styles.container_input }>
-        <input
-        className={styles.input}
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder='Телефон'
-                
-          value={formData.phone}
-          onChange={handleChange}
-        />
-        <input
-        className={styles.input}
-          type="email"
-          id="email"
-          name="email"
-          placeholder='Email'
-          value={formData.email}
-          onChange={handleChange}
-        />
-        </div>
-        <div className={styles.delivery}>
-          <p className={styles.title}>Доставка</p> 
-          <p className={styles.title}>Спосіб оплати</p>
-        </div>
-          <div>
-            <MeMoNovaPost />
-        </div>
-    </form>
-    </div>
-        <div className={styles.container_cards}>
-          <h2 className={styles.title} style={{ color: "#FAF7F1", paddingBottom: "33px" }}>Ваше замовлення</h2>
-          <div className={styles.container_contant}>
-            <div className={styles.item_description}>
-              <p className={styles.item_text}>Кількість</p>
-              <p className={styles.item_text}>Сума</p>
-            </div> 
-            <ul className={styles.list}>
-              {storedItems.map(({ImageCards, TitleCards, PriceCards, QuantityCards}, id) => (
-                <li key={id} className={styles.item}>
-                  <Image
-                    src={ImageCards}
-                    alt="Image"
-                    priority={true}
-                    loading="eager"
-                    quality={100}
-                    width={100}
-                    height={100}
-                    className={styles.item_img}
-                    />
-                  <p className={styles.item_title}>{TitleCards}</p>
-                  <div className={styles.item_quantity}>
-                    <div className={styles.container_quantity}>
-                        <p className={styles.text}>{QuantityCards}</p>
-                    </div>
-                  </div>
-                  <div className={styles.item_container_price}>
-                    <p className={styles.price}>{PriceCards} гривень</p>
-                  </div>
-              </li>
-              ))}
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <div className={styles.container_form}>
+            <h2 className={styles.title}>Ваші контактні дані</h2>
 
-            </ul>
-            <div className={styles.container_price}>
-              <p className={styles.text}>Підсумок</p>
-              <p className={styles.price}>{totalCardPrice} ГРИВЕНЬ</p>
+            <form onSubmit={handleSubmit} className={styles.form}>
+              <div className={styles.container_input}>
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  placeholder="Ім’я"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Прізвище"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className={styles.container_input}>
+                <input
+                  className={styles.input}
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="Телефон"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+                <input
+                  className={styles.input}
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className={styles.delivery}>
+                <p className={styles.title}>Доставка</p>
+                <p className={styles.title}>Спосіб оплати</p>
+              </div>
+              <div>
+                <MeMoNovaPost />
+              </div>
+            </form>
+          </div>
+          <div className={styles.container_cards}>
+            <h2
+              className={styles.title}
+              style={{ color: "#FAF7F1", paddingBottom: "33px" }}
+            >
+              Ваше замовлення
+            </h2>
+            <div className={styles.container_contant}>
+              <div className={styles.item_description}>
+                <p className={styles.item_text}>Кількість</p>
+                <p className={styles.item_text}>Сума</p>
+              </div>
+              <ul className={styles.list}>
+                {storedItems.map(
+                  (
+                    { ImageCards, TitleCards, PriceCards, QuantityCards },
+                    id
+                  ) => (
+                    <li key={id} className={styles.item}>
+                      <Image
+                        src={ImageCards}
+                        alt="Image"
+                        priority={true}
+                        loading="eager"
+                        quality={100}
+                        width={100}
+                        height={100}
+                        className={styles.item_img}
+                      />
+                      <p className={styles.item_title}>{TitleCards}</p>
+                      <div className={styles.item_quantity}>
+                        <div className={styles.container_quantity}>
+                          <p className={styles.text}>{QuantityCards}</p>
+                        </div>
+                      </div>
+                      <div className={styles.item_container_price}>
+                        <p className={styles.price}>
+                          {PriceCards * QuantityCards} гривень
+                        </p>
+                      </div>
+                    </li>
+                  )
+                )}
+              </ul>
+              <div className={styles.container_price}>
+                <p className={styles.text}>Підсумок</p>
+                <p className={styles.price}>{totalCardPrice} ГРИВЕНЬ</p>
+              </div>
+            </div>
+
+            <div className={styles.container_btn}>
+              <Liqpay />
+              <button
+                className={styles.btn_edit}
+                onClick={() => setIsPay(true)}
+              >
+                редагувати замовлення
+              </button>
             </div>
           </div>
-
-          <div className={styles.container_btn}>
-            <Liqpay />
-            <button className={styles.btn_edit} onClick={()=> setIsPay(true)}>редагувати замовлення</button>
-          </div>
         </div>
-      </div>
-    </section>
-      {isPay && <PayModal
-        setIsPay={setIsPay}
-        storedItems={storedItems}
-        setStoredItems={setStoredItems}
-      />}
+      </section>
+      {isPay && (
+        <PayModal
+          setIsPay={setIsPay}
+          storedItems={storedItems}
+          setStoredItems={setStoredItems}
+        />
+      )}
     </>
-  )
+  );
 }
