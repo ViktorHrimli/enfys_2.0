a30671db438c59cb09d072411361c0c42b0310cc
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2024-06-03 22:40:50 +0200

    fix corner

--- src/libs/modal/thank-you/thank-you.jsx
+++ src/libs/modal/thank-you/thank-you.jsx
@@ -1,31 +1,25 @@
 import styles from './thank-you.module.scss'
 import Image from 'next/image'
 import closed from '@/assets/svg/closed.svg';
+
 import cornerRightOrange from '@/assets/svg/corner-right-orange.svg';
 import cornerLeftOrange from '@/assets/svg/corner-left-orange.svg';
-
 
 
 export default function ThenkYou({setIsThenkyou}) {
   return (
     <div className={styles.container}>
+       
       <div className={styles.contant}>
       <div className={styles.corner_left}>
           <Image
             src={cornerLeftOrange}
-            alt="cornerLeft"
+            alt="pay"
             priority={true}
             loading="eager"
           />
         </div>
-        <div className={styles.corner_right}>
-          <Image
-            src={cornerRightOrange}
-            alt="cornerRight"
-            priority={true}
-            loading="eager"
-          />
-        </div>
+       
         <div className={styles.closed} onClick={() => setIsThenkyou(false)}>
             <Image
               src={closed}
@@ -37,6 +31,18 @@ export default function ThenkYou({setIsThenkyou}) {
         <h2 className={styles.header}>дякуємо, ваш запит успішно надісланий</h2>
         <p className={styles.inform}>Ми зв’яжемося з Вами якомога швидше. Гарного дня!</p>
         <p className={styles.post_scriptum}>P.S. Пам’ятайте, ви - прекрасні батьки для своєї дитини.</p>
+        <div>
+          {/* qwertyui */}
+        </div>
+        <div className={styles.corner_right}>
+          <Image
+            src={cornerRightOrange}
+            alt="pay"
+            priority={true}
+            loading="eager"
+          />
+        </div>
+       
       </div>
     </div>
 
