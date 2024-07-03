2b6ac006de3a76d456baae7e6e617f99133a128f
Savchenko-Andrey <dracxm95@gmail.com>
2024-07-03 10:55:51 +0300

    fix scroll positions page

--- src/shared/hook/useScrollPosition.js
+++ src/shared/hook/useScrollPosition.js
@@ -12,9 +12,16 @@ const useScrollPosition = (path) => {
 
     const handleLoad = () => {
       const savedScrollPosition = localStorage.getItem(path);
+      console.log(savedScrollPosition);
+
       if (savedScrollPosition !== null) {
         window.scrollTo(0, parseInt(savedScrollPosition, 10));
       }
+
+      const time = setTimeout(() => {
+        window.scrollTo(0, parseInt(savedScrollPosition, 10));
+        clearTimeout(time);
+      }, 5000);
     };
 
     window.addEventListener("load", handleLoad);
